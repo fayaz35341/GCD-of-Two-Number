0e62548f7952a97263154fb0b1d5d3be6a0a5e2e
Riyaz shaik <94755295+fayaz35341@users.noreply.github.com>
2025-06-09 11:16:01 +0530

    Update GCD of two Numbers.js

--- GCD of two Numbers.js
+++ GCD of two Numbers.js
@@ -1,15 +1,27 @@
-class Solution {
-    gcd(a, b) {
-        // code here
-        let c1=[]
-        for (let i=1;i<=Math.min(a,b);i++){
-            if (a%i===0 && b%i===0){
-                c1.push(i)
-            }
+// class Solution {
+//     gcd(a, b) {
+//         // code here
+//         let c1=[]
+//         for (let i=1;i<=Math.min(a,b);i++){
+//             if (a%i===0 && b%i===0){
+//                 c1.push(i)
+//             }
+//         }
+//         return Math.max(...c1)
+//     }
+// }
+// console.log(new Solution().gcd(4,6))
+
+// another
+class Solution{
+    gcd(a,b){
+result=1
+        while (b!==0){
+            let temp=b
+            b=a%b
+            a=temp
         }
-        return Math.max(...c1)
+        return a
     }
 }
 console.log(new Solution().gcd(4,6))
-
-// another
