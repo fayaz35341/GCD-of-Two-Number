f1282f0f1d6dd601d7e74962ae93208a491431db
Riyaz shaik <94755295+fayaz35341@users.noreply.github.com>
2025-06-09 11:12:37 +0530

    Update GCD of two Numbers.py

--- GCD of two Numbers.py
+++ GCD of two Numbers.py
@@ -1,12 +1,18 @@
+# class Solution:
+#     def GCD(self, n1, n2):
+#         count1=[]
+#         count2=[]
+#         for i in range(1,n1+1):
+#             if n1%i==0:
+#                 count1.append(i)
+#         for j in range(1,n2+1):
+#             if n2%j==0:
+#                 count2.append(j)
+#         return max(set(count1) & set(count2))
+# print(Solution().GCD(4,6))
+
+import math
 class Solution:
     def GCD(self, n1, n2):
-        count1=[]
-        count2=[]
-        for i in range(1,n1+1):
-            if n1%i==0:
-                count1.append(i)
-        for j in range(1,n2+1):
-            if n2%j==0:
-                count2.append(j)
-        return max(set(count1) & set(count2))
+        return math.gcd(n1,n2)
 print(Solution().GCD(4,6))
